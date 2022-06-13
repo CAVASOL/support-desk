0454261a3f2b94647dc148042254e84ea4ec20f3
Yeon Kim <awyeon@gmail.com>
2022-06-13 21:32:23 +0800

    testing/fetching notes data

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -51,12 +51,10 @@ function Ticket() {
       <div className='ticket-page'>
         <header className='ticket-header'>
           <BackButton url='/tickets' />
-          <h2>
-            Ticket ID: { ticket._id }
-            <span className={`status status-${ticket.status}`}>{ticket.status}</span>
-          </h2>
+
+          <h3>Ticket ID: { ticket._id }</h3>
           <h3>Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}</h3>
-          <h3>Product: {ticket.product}</h3>
+          <h3>Product: {ticket.product}  <span className={`status status-${ticket.status}`}>{ticket.status}</span></h3>
           <hr />
           <div className='ticket-desc'>
             <h3>Description of Issue</h3>
