5c696dc40046908d26904b906fc14b07acce2292
Yeon Kim <awyeon@gmail.com>
2022-06-14 12:45:46 +0800

    testing

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
+// Connect to database
 connectDB()
 
 const app = express()
@@ -17,10 +18,15 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
+// Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
-  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
 } else {
   app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the Support Desk API' })
@@ -28,4 +34,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(errorHandler)
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
